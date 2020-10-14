8f5e282512cd0537089cb2849aa98e4fd8d1074c
jasonleow <pleasegetjason@gmail.com>
2020-10-14 18:02:35 +0800

    Deleted env production database.js folder and file, and tried if else for config database.js file

--- config/database.js
+++ config/database.js
@@ -1,15 +1,39 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+  if (env('NODE_ENV') === 'development'){
+    return {
+      defaultConnection: 'default',
+      connections: {
+        default: {
+          connector: 'bookshelf',
+          settings: {
+            client: 'sqlite',
+            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+          },
+          options: {
+            useNullAsDefault: true,
+          },
+        },
       },
-      options: {
-        useNullAsDefault: true,
-      },
-    },
-  },
-});
+    }
+  } else {
+      return {
+        defaultConnection: 'default',
+        connections: {
+          default: {
+            connector: 'bookshelf',
+            settings: {
+              client: 'postgres',
+              host: config.host,
+              port: config.port,
+              database: config.database,
+              username: config.user,
+              password: config.password,
+            },
+            options: {
+              ssl: false,
+            },
+          },
+        },
+      }
+    }
+})  
