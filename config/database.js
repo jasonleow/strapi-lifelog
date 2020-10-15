c627d7aef0de588b5b64389072e87a39cec56dec
jasonleow <pleasegetjason@gmail.com>
2020-10-15 09:56:31 +0800

    Added .env file

--- config/database.js
+++ config/database.js
@@ -1,42 +1,15 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-
 module.exports = ({ env }) => ({
-  if (env('NODE_ENV') === 'development'){
-    return {
-      defaultConnection: 'default',
-      connections: {
-        default: {
-          connector: 'bookshelf',
-          settings: {
-            client: 'sqlite',
-            filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-          },
-          options: {
-            useNullAsDefault: true,
-          },
-        },
+  defaultConnection: 'default',
+  connections: {
+    default: {
+      connector: 'bookshelf',
+      settings: {
+        client: 'sqlite',
+        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
       },
-    }
-  } else {
-      return {
-        defaultConnection: 'default',
-        connections: {
-          default: {
-            connector: 'bookshelf',
-            settings: {
-              client: 'postgres',
-              host: config.host,
-              port: config.port,
-              database: config.database,
-              username: config.user,
-              password: config.password,
-            },
-            options: {
-              ssl: false,
-            },
-          },
-        },
-      }
-    }
-})  
+      options: {
+        useNullAsDefault: true,
+      },
+    },
+  },
+});
