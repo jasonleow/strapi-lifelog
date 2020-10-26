e5a27e21a1446c095c235edb37f25bda4c2153b9
jasonleow <pleasegetjason@gmail.com>
2020-10-26 12:22:59 +0800

    Added create is owner policy for posts, and changed users-permissions_user to author

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -14,13 +14,55 @@ module.exports = {
     return sanitizeEntity(entity, { model: strapi.models.post });
   },
 
+  // when a post is created, the authenticated user is automatically set as author of the post
+  async create(ctx) {
+    let entity;
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      data.author = ctx.state.user.id;
+      entity = await strapi.services.post.create(data, { files });
+    } else {
+      ctx.request.body.author = ctx.state.user.id;
+      entity = await strapi.services.post.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+
+  // restrict the update of posts only for the author
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [post] = await strapi.services.post.find({
+      id: ctx.params.id,
+      'author.id': ctx.state.user.id,
+    });
+
+    if (!post) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is('multipart')) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.post.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.post.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+
+  //create comment for posts
   async comment(ctx) {
     let entity;
     if (ctx.is('multipart')) {
       const { data, files } = parseMultipartData(ctx);
       entity = await strapi.services.comment.create(data, { files });
     } else {
-    	ctx.request.body.user = ctx.state.user.id;
+    	ctx.request.body.author = ctx.state.user.id;
     	ctx.request.body.post = ctx.params.id;
       entity = await strapi.services.comment.create(ctx.request.body);
     }
